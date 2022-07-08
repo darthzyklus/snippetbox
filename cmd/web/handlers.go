9c4cbe7b14b6f82a8ecae90992367553eb8888b4
Andres Uris <andres.urismontilla@gmail.com>
2022-07-08 01:08:10 -0400

    refactor project structure and organization

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(writer http.ResponseWriter, req *http.Request) {
-
 	if req.URL.Path != "/" {
 		http.NotFound(writer, req)
 		return
@@ -31,22 +29,9 @@ func snippetView(writer http.ResponseWriter, req *http.Request) {
 func snippetCreate(writer http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodPost {
 		writer.Header().Set("Allow", http.MethodPost)
-		http.Error(writer, "Method Not Allowed", http.StatusMethodNotAllowed)
+		http.Error(writer, "Method Not Allowed", http.StatusUnsupportedMediaType)
 		return
 	}
 
-	writer.Write([]byte("Create a new snippet..."))
-}
-
-func main() {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-
-	log.Println("Starting server on port 4000")
-
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	writer.Write([]byte("Create a snippet..."))
 }
