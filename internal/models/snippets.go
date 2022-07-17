2d61618de48ac3c8d82e49671e7a493050420c2a
Andres Uris <andres.urismontilla@gmail.com>
2022-07-16 23:40:33 -0400

    use transaction when inserting a snippet

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -19,10 +19,24 @@ type SnippetModel struct {
 }
 
 func (model *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+	tx, err := model.DB.Begin()
+
+	if err != nil {
+		return 0, err
+	}
+
+	defer tx.Rollback()
+
 	stmt := `INSERT INTO snippets (title, content, created, expires)
 		VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))
 	`
-	result, err := model.DB.Exec(stmt, title, content, expires)
+	result, err := tx.Exec(stmt, title, content, expires)
+
+	if err != nil {
+		return 0, err
+	}
+
+	err = tx.Commit()
 
 	if err != nil {
 		return 0, err
