0b87f34c3d9ee9f69cd40d01bf6a1e38558713b6
Andres Uris <andres.urismontilla@gmail.com>
2024-05-16 21:49:20 +0200

    refactor code to new structure

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"strconv"
+    "net/http"
+    "strconv"
+    "fmt"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -30,17 +29,4 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusCreated)
     w.Write([]byte("Save a new snippet..."))
-}
-
-func main() {
-    mux := http.NewServeMux()
-    mux.HandleFunc("GET /{$}", home)
-    mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-    mux.HandleFunc("GET /snippet/create", snippetCreate)
-    mux.HandleFunc("POST /snippet/create", snippetCreatePost)
-
-    log.Println("starting server on port :4000")
-
-    err := http.ListenAndServe(":4000", mux)
-    log.Fatal(err)
 }
