c4bd4a30f86987ee3a43a6d84d4cf9d56c347e77
Andres Uris <andres.urismontilla@gmail.com>
2024-05-18 14:27:30 +0200

    break html home page into template parts

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,13 +1,37 @@
 package main
 
 import (
-    "net/http"
-    "strconv"
-    "fmt"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
     w.Header().Add("Server", "Go")
+
+    files := []string{
+        "./ui/html/base.tmpl",
+        "./ui/html/pages/home.tmpl",
+    }
+
+    ts, err := template.ParseFiles(files...)
+
+    if err != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
+
+    err = ts.ExecuteTemplate(w, "base", nil)
+
+    if err != nil {
+        log.Print(err.Error())
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
+
     w.Write([]byte("Hello from Snippetbox"))
 }
 
